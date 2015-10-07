bc17907bed9486285c71c6544434b722b4392e76
Eugene Kuzmenko <aljey1972@gmail.com>
2015-10-07 14:34:14 +0300

    [fixed] only run tests after a successful build

--- bin/build.js
+++ bin/build.js
@@ -4,19 +4,20 @@ import {JS, NativeProcess, SASSLint} from 'webcompiler';
 import {join} from 'path';
 
 const rootDir = join(__dirname, '..'),
-    modulesDir = join(rootDir, 'node_modules'),
     binDir = join(rootDir, 'bin'),
     buildDir = join(rootDir, 'build'),
     libDir = join(rootDir, 'lib'),
     devDir = join(rootDir, 'development'),
     docsDir = join(rootDir, 'docs'),
     specDir = join(rootDir, 'spec'),
+    modulesDir = join(rootDir, 'node_modules'),
     readme = join(rootDir, 'README.md'),
     jsdocConfig = join(modulesDir, 'webcompiler', 'config', 'jsdoc.json'),
     style = join(rootDir, '_index.scss'),
     devStyle = join(devDir, 'app.scss'),
     js = new JS(),
-    jsdoc = new NativeProcess(join(modulesDir, '.bin', 'jsdoc'));
+    jsdoc = new NativeProcess(join(modulesDir, '.bin', 'jsdoc')),
+    npm = new NativeProcess('npm');
 
 new SASSLint().run([style, devStyle], function () {
   js.beDir(libDir, buildDir, function () {
@@ -25,6 +26,7 @@ new SASSLint().run([style, devStyle], function () {
         return console.error(e);
       }
       console.log('\x1b[32mGenerated API documentation!\x1b[0m');
+      npm.run(Function.prototype, ['test'], {stdio: 'inherit'});
     }, [buildDir, '-d', docsDir, '-R', readme, '-c', jsdocConfig]);
   }, specDir, binDir, devDir);
 });
